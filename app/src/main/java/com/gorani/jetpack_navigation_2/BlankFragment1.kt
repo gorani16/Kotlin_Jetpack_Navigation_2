487addf66f8b2635c7684fb125fffaa392f07288
gorani16 <jgee0116@naver.com>
2023-03-05 23:26:13 +0900

    Exercise Jetpack Navigation Safe Args

--- app/src/main/java/com/gorani/jetpack_navigation_2/BlankFragment1.kt
+++ app/src/main/java/com/gorani/jetpack_navigation_2/BlankFragment1.kt
@@ -13,8 +13,10 @@ import androidx.navigation.Navigation
  * Fragment 간 Data 를 전달하는 방법.
  * Key 와 Value 의 한 쌍으로 Data 를 Bundle 객체에 담아 다른 Fragment 로 전달한다.
  * 단순히 Bundle 객체를 만들어 Data 를 보내는 방식의 경우 받은 Data 의 값이 null 일 수도 있다.
- * Data 의 값이 null 인 경우 반드시 예외처리를 해줘야하는 수고가 든다.
+ * Data 의 값이 null 인 경우 반드시 예외처리를 해줘야하는 수고가 발생한다.
  * 이런 문제점을 고려하여 Google 공식 문서에서는 Safe Args 를 사용하기를 권장한다.
+ *
+ * https://developer.android.com/guide/navigation/navigation-pass-data?hl=ko
  *
  */
 
@@ -30,14 +32,25 @@ class BlankFragment1 : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Bundle 생성
-        val bundle = Bundle()
-        bundle.putString("key", "value")
+//        // * 방법1. 일반적인 Data 전달 방법 : Bundle
+//        // Bundle 생성
+//        val bundle = Bundle()
+//        bundle.putString("key", "value")
+//
+//        // Bundle 객체 보내기
+//        view.findViewById<Button>(R.id.btn_1).setOnClickListener {
+//            Navigation.findNavController(it).navigate(R.id.action_blankFragment1_to_blankFragment2, bundle)
+//        }
 
-        // Bundle 객체 보내기
+        // * 방법2. Safe Args
+//        val action = BlankFragment1Directions.actionBlankFragment1ToBlankFragment2("abcdef")    // Data (abcdf) 를 전달
+        val action = BlankFragment1Directions.actionBlankFragment1ToBlankFragment2()
+        // ㄴ> Data 없이 전달 -> Default 값이 전달됨. 이런 측면에서 Safe 하다고 할 수 있음. (적어도 Nullable 이 아니기 때문.)
+
         view.findViewById<Button>(R.id.btn_1).setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.action_blankFragment1_to_blankFragment2, bundle)
+            Navigation.findNavController(it).navigate(action)
         }
+
     }
 
 }
--- app/src/main/java/com/gorani/jetpack_navigation_2/BlankFragment2.kt
+++ app/src/main/java/com/gorani/jetpack_navigation_2/BlankFragment2.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
 
 class BlankFragment2 : Fragment() {
 
@@ -19,8 +22,19 @@ class BlankFragment2 : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Bundle 객체의 데이터 받기
-        val bundleData = arguments?.getString("key")    // key : Fragment1 에서 생성한 Bundle 객체의 Key 값
-        Log.d("bundleData", "$bundleData")
+//        // 방법1. Bundle 객체의 데이터 받기
+//        val bundleData = arguments?.getString("key")    // key : Fragment1 에서 생성한 Bundle 객체의 Key 값
+//        Log.d("bundleData", "$bundleData")
+
+        // 방법2. Safe Args 데이터 받기
+        val args: BlankFragment2Args by navArgs()
+        Log.d("Safe_Args", args.key)
+
+        view.findViewById<Button>(R.id.btn_2).setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_blankFragment2_to_blankFragment1)
+        }
+
+
+
     }
 }
